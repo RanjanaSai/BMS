5a4b8ef2c1497fb781858f48bbc76893e28c3511
RanjanaSai <ranjana601@gmail.com>
2026-05-27 15:44:02 +0530

    change in app.js

--- server/app.js
+++ server/app.js
@@ -36,10 +36,10 @@ app.use(express.json());//parse incoming json request
 //apply rate limiter middleware
 // app.use('/api/', apiLimiter);
 
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*splat', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-})
+// app.use(express.static(path.join(__dirname, 'build')));
+// app.get('/*splat', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// })
 
 
 app.use('/api/user', userRoute)
