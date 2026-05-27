7e0865db55be6b9f8bafdc3cb0794c9ea341faa5
RanjanaSai <ranjana601@gmail.com>
2026-05-27 11:06:49 +0530

    changes in app.js for app.get*

--- server/app.js
+++ server/app.js
@@ -37,7 +37,7 @@ app.use(cors());// allowing CORS requests
 // app.use('/api/', apiLimiter);
 
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('*', (req, res) => {
+app.get('*/:splat', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 
